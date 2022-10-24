e4c76b8b7d7acdc639ed351389a1f2bdb9a30e69
1e10 <sample@bths.edu>
2022-10-24 10:45:21 -0400

    first commit

---
+++ src/Main.java
@@ -0,0 +1,99 @@
+public class Main {
+    public static void main(String args[]){
+//        BooleanGames game1 = new BooleanGames(10);
+//        System.out.println(game1.isNegative());
+//        System.out.println("----");
+//        System.out.println(game1.exceeds(9));
+//        System.out.println(game1.exceeds(10));
+//        System.out.println(game1.exceeds(11));
+//        System.out.println("----");
+//        System.out.println(game1.isEven());
+//        System.out.println(game1.isOdd());
+//        System.out.println("----");
+//        System.out.println(game1.isMultipleOf(1));
+//        System.out.println(game1.isMultipleOf(2));
+//        System.out.println(game1.isMultipleOf(5));
+//        System.out.println(game1.isMultipleOf(10));
+//        System.out.println(game1.isMultipleOf(4));
+//        System.out.println(game1.isMultipleOf(8));
+//        System.out.println(game1.isMultipleOf(20));
+//        System.out.println("----");
+//        System.out.println(game1.isOnesDigit(0));
+//        System.out.println(game1.isOnesDigit(1));
+//        System.out.println(game1.isOnesDigit(5));
+////
+////
+////        Test Case 2: (be sure methods handle negatives!)
+//        BooleanGames game1 = new BooleanGames(-15);
+//        System.out.println(game1.isNegative());
+//        System.out.println("----");
+//        System.out.println(game1.exceeds(-20));
+//        System.out.println(game1.exceeds(-15));
+//        System.out.println(game1.exceeds(0));
+//        System.out.println("----");
+//        System.out.println(game1.isEven());
+//        System.out.println(game1.isOdd());
+//        System.out.println("----");
+//        System.out.println(game1.isMultipleOf(1));
+//        System.out.println(game1.isMultipleOf(3));
+//        System.out.println(game1.isMultipleOf(5));
+//        System.out.println(game1.isMultipleOf(15));
+//        System.out.println(game1.isMultipleOf(10));
+//        System.out.println(game1.isMultipleOf(2));
+//        System.out.println(game1.isMultipleOf(30));
+//        System.out.println("----");
+//        System.out.println(game1.isOnesDigit(5));
+//        System.out.println(game1.isOnesDigit(1));
+//        System.out.println(game1.isOnesDigit(6));
+
+////        Test Case 3:
+//        BooleanGames game1 = new BooleanGames(7283);
+//        System.out.println(game1.isNegative());
+//        System.out.println("----");
+//        System.out.println(game1.exceeds(7000));
+//        System.out.println(game1.exceeds(7283));
+//        System.out.println(game1.exceeds(7500));
+//        System.out.println("----");
+//        System.out.println(game1.isEven());
+//        System.out.println(game1.isOdd());
+//        System.out.println("----");
+//        System.out.println(game1.isMultipleOf(1));
+//        System.out.println(game1.isMultipleOf(3));
+//        System.out.println(game1.isMultipleOf(7));
+//        System.out.println(game1.isMultipleOf(13));
+//        System.out.println(game1.isMultipleOf(10));
+//        System.out.println("----");
+//        System.out.println(game1.isOnesDigit(3));
+//        System.out.println(game1.isOnesDigit(8));
+//        System.out.println(game1.isOnesDigit(2));
+//        System.out.println(game1.isOnesDigit(7));
+//        System.out.println(game1.isOnesDigit(5));
+
+
+//        Test Case 4:
+        BooleanGames game1 = new BooleanGames(-435);
+        System.out.println(game1.isNegative());
+        System.out.println("----");
+        System.out.println(game1.exceeds(-600));
+        System.out.println(game1.exceeds(-500));
+        System.out.println(game1.exceeds(0));
+        System.out.println("----");
+        System.out.println(game1.isEven());
+        System.out.println(game1.isOdd());
+        System.out.println("----");
+        System.out.println(game1.isMultipleOf(1));
+        System.out.println(game1.isMultipleOf(2));
+        System.out.println(game1.isMultipleOf(3));
+        System.out.println(game1.isMultipleOf(5));
+        System.out.println(game1.isMultipleOf(35));
+        System.out.println(game1.isMultipleOf(145));
+        System.out.println(game1.isMultipleOf(435));
+        System.out.println("----");
+        System.out.println(game1.isOnesDigit(5));
+        System.out.println(game1.isOnesDigit(3));
+        System.out.println(game1.isOnesDigit(4));
+        System.out.println(game1.isOnesDigit(0));
+        System.out.println(game1.isOnesDigit(35));
+
+    }
+}
